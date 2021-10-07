84e6ae4439704560ba3aa62069184e19d3964fc9
Ratarca <ratarca.rtc@gmail.com>
2021-10-07 19:28:33 -0300

    🚧

---
+++ 0 - Aulas/1_oop/20_1_composition.py
@@ -0,0 +1,27 @@
+"""
+"""
+
+class Flashlight():
+    def __init__(self):
+        pass
+
+    def lighten(self,objectTarget:str):
+        var = f"LIGHTEN : {objectTarget}"
+        return var
+class Scout():
+    def __init__(self,name:str, flash: Flashlight):
+        self.name = name
+        self.flash = flash
+
+    def speak(self, Text:str):
+        print(f"{Text}")
+
+    def light(self, target):
+        return self.flash.lighten(target)
+
+fenix = Flashlight()
+ratarca = Scout("Ratarca",fenix)
+print( ratarca.light('MONKEY') )
+
+
+# TIP : Dependency Inversion Principle
