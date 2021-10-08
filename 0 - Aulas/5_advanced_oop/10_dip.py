6d72c1904b6926b7c5954f6b952aca0703cac006
Ratarca <ratarca.rtc@gmail.com>
2021-10-08 11:09:50 -0300

    🍻

--- 0 - Aulas/5_advanced_oop/10_dip.py
+++ 0 - Aulas/5_advanced_oop/10_dip.py
@@ -17,4 +17,22 @@ class ICool(metaclass=ABCMeta):
         pass
 
 # Low-level
+class LowOne(ICool):
+    pass
+
+    def nothing():
+        pass
+
+class Lowtwo(ICool):
+    pass
+
+    def nothing():
+        pass
+
 # High-level
+class HighOne():
+    def __init__(self, objectCool):
+        self.objectCool = objectCool
+    
+    def calculate():
+        return self.objectCool.nothing()
