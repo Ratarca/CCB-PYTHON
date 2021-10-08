3739a7a47ef1e3fba76278e633d0563536bb4f06
Ratarca <ratarca.rtc@gmail.com>
2021-10-08 12:15:46 -0300

    🍻

--- 0 - Aulas/4_files/00_generate.py
+++ 0 - Aulas/4_files/00_generate.py
@@ -13,5 +13,25 @@ filePath = lambda : os.path.dirname(sys.argv[0])
 createData = lambda : {"ID":random.randint(0, 3**7), "LIFE":random.randint(0, 3**7)}
 
 # Write line by line
+with open(filePath() + "\\fileOne.csv", 'w' ) as fp:
+    writer = csv.writer(fp)
+    data = list(createData().values())
+    writer.writerow( data )
+
 # Write many rows
+with open(filePath() + "\\fileTwo.csv", 'w' ,encoding='UTF-8') as fp:
+    writer = csv.writer(fp)
+
+    writer.writerow(['ID','LIFE']) #Create header
+
+    data = [ list(createData().values()) for x in range(10)]
+    writer.writerows( data )
+
 # Write by dict
+with open(filePath() + "\\fileThree.csv", 'w' ,encoding='UTF8', newline='') as fp:
+    writer = csv.DictWriter(fp,fieldnames=['ID','LIFE'])
+
+    writer.writeheader()
+
+    data = [ list(createData().values()) for x in range(10)]
+    writer.writerows(data)
